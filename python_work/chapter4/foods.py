e36a343b2cbf37007e40dac85e28a39b754cd07d
WagnerSF10 <wagner.franca@fatec.sp.gov.br>
2020-07-09 19:24:47 -0300

    Copiando uma lista

--- python_work/chapter4/foods.py
+++ python_work/chapter4/foods.py
@@ -1,1 +1,8 @@
+my_foods=['pizza','falafel','carrot cake']
+friend_foods=my_foods[:]
+#Copiando uma lista
+print("My favorite foods are:")
+print(my_foods)
 
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
