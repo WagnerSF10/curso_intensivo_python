c5046789b82d11b9cbbba9121dcdcb98a07151c2
WagnerSF10 <wagner.franca@fatec.sp.gov.br>
2020-09-06 10:48:09 -0300

    Mais arquivos do capítulo 6

--- python_work/chapter6/favorite_languages.py
+++ python_work/chapter6/favorite_languages.py
@@ -9,4 +9,52 @@ favorite_languages = {
 
 print("Sarah's favorite language is " +
     favorite_languages['sarah'].title() + 
-    ".")
+    ".\n")
+
+#Percorrendo todos os pares chave-valor com um laço
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+        language.title() + ".")
+
+#Percorrendo todas as chaves de um dicionário com um laço
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+#Ou podemos poercorrer sem KEYS
+
+for name in favorite_languages:
+    print(name.title())
+    
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    
+    if name in friends:
+        print(" Hi " + name.title() +
+            ", I see your favorite language is " +
+            favorite_languages[name].title() + "!"
+            )
+
+if 'erin' not in favorite_languages.keys():
+    print("\nErin, please take our poll!\n")
+
+#Percorrendo as chaves de um dicionário em ordem usando um laço
+
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+#Percorrendo todos os valores de um dicionário com um laço
+
+print("\nThe following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+#Exibindo nomes sem repetições com SET
+
+print("\nThe following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+
